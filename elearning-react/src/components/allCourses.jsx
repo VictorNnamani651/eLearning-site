55a269c1f78cb2b30dedf7ecd22372a0135035d9
Victor Nnamani <victornnamani651@gmail.com>
2025-07-21 07:10:31 +0100

    minor style adjustment

--- elearning-react/src/components/allCourses.jsx
+++ elearning-react/src/components/allCourses.jsx
@@ -8,7 +8,7 @@ function AllCourses() {
       <SearchBar />
       <div className="border border-2 border-info row d-flex gap-3 justify-content-center py-3">
         {courseData.map((course) => (
-          <CourseCard course={course} columnClass={"col-12 col-md-3"} />
+          <CourseCard course={course} columnClass={"col-12 col-md-3 p-0"} />
         ))}
       </div>
     </>
