0b1c5c30040a5059bcec85f97487a9819a18d491
Victor Nnamani <victornnamani651@gmail.com>
2025-07-25 09:55:09 +0100

    Created a backend simulator with JSON server. After setting this up i used a json file as a dummy database. This also led to me setting up an API to call for data from the dummy db.

--- elearning-react/src/api/api.js
+++ elearning-react/src/api/api.js
@@ -1,11 +1,12 @@
-import axios from "axios";
+const BASE_URL = import.meta.env.VITE_API_BASE;
 
-const api = axios.create({
-  baseURL: "http://localhost:3001",
-  timeout: 5000,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-export default api;
+export const API = {
+  USERS: `${BASE_URL}/users`,
+  COURSES: `${BASE_URL}/courses`,
+  POPULAR_COURSES: `${BASE_URL}/popularCourses`,
+  INSTRUCTORS: `${BASE_URL}/instructors`,
+  NEWS: `${BASE_URL}/news`,
+  FOOTER_LINKS: `${BASE_URL}/footerLinks`,
+  FOOTER_GALLERY: `${BASE_URL}/footerGallery`,
+  TESTIMONIALS: `${BASE_URL}/testimonials`,
+};
