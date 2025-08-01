f83b36d1ec006082e9820858d775ea1d4667ed3a
Victor Nnamani <victornnamani651@gmail.com>
2025-08-01 15:22:39 +0100

    Fetch news data from API and implement loading state in NewsAndTestimonialsSection

--- elearning-react/src/components/news&testimony.jsx
+++ elearning-react/src/components/news&testimony.jsx
@@ -1,9 +1,27 @@
-
-import NewsCard from './newsCard';
-import TestimonialCard from './testimonialCard';
-import { newsData } from '../db';
+import { useEffect, useState } from "react";
+import NewsCard from "./newsCard";
+import TestimonialCard from "./testimonialCard";
+import { API } from "../api/api";
+import LoadingScreen from "./loading";
 
 const NewsAndTestimonialsSection = () => {
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const res = await fetch(API.NEWS);
+        const data = await res.json();
+        setNews(data);
+      } catch (err) {
+        console.error("Failed to fetch news:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNews();
+  }, []);
+
   return (
     <section className="py-5" id="news-and-testimony">
       <div className="container-md">
@@ -11,9 +29,11 @@ const NewsAndTestimonialsSection = () => {
           <div className="col-md p-md-5 mb-3 mb-md-0" id="news">
             <h3 className="mb-5">Latest News</h3>
             <div className="events">
-              {newsData.map((news) => (
-                <NewsCard key={news.id} news={news} />
-              ))}
+              {loading ? (
+                <LoadingScreen />
+              ) : (
+                news.map((news) => <NewsCard key={news.id} news={news} />)
+              )}
             </div>
           </div>
           <div className="col-md p-md-5" id="testimonials">
