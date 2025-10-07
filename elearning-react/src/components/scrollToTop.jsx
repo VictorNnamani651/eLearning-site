90c7b9899fd19e37410550ad62de375ca4ab80be
Victor Nnamani <victornnamani651@gmail.com>
2025-10-07 23:04:15 +0100

    Refactored ScrollToTop component to fix bug caused by bootstrap. when a component like a modal, offcanvas sidebar, or dropdown is opened, to prevent background scrolling bootstrap does this --> body {   overflow: hidden;   padding-right: 0px; }
    It is removed when the component closes via Bootstrap’s JavaScript.
    However, in single-page apps like React + React Router, if you navigate away before Bootstrap’s cleanup runs, the overflow: hidden stays stuck.
    
    That causes the scroll to freeze after route changes.

--- elearning-react/src/components/scrollToTop.jsx
+++ elearning-react/src/components/scrollToTop.jsx
@@ -6,7 +6,12 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
+    // Reset scroll position
     window.scrollTo(0, 0);
+
+    // Force-remove Bootstrap's leftover styles
+    document.body.style.overflow = "auto";
+    document.body.style.paddingRight = "0px";
   }, [pathname]);
 
   return null;
